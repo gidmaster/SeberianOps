8e52aa905bb13d5ffea4d58cd9abbf1482049021
Konstantin Syrovatsky <k.syrovatsky@gmail.com>
2026-02-25 18:26:28 +0000

    fix: Fix initial certificate issue.

--- scripts/init-certbot.sh
+++ scripts/init-certbot.sh
@@ -4,18 +4,33 @@
 DOMAIN="siberianops.gidmaster.dev"
 EMAIL="k.syrovatsky@gmail.com"
 
-# Start nginx first (needed for acme-challenge)
+echo ">>> Creating data directories"
+mkdir -p data/certbot/conf data/certbot/www
+
+echo ">>> Starting Nginx with HTTP-only config"
+cp docker/nginx/nginx.conf docker/nginx/nginx.conf.backup
+cp docker/nginx/nginx.conf.init docker/nginx/nginx.conf
+
 docker compose -f docker-compose.prod.yml up -d nginx
 
-# Get certificate
-docker compose -f docker-compose.prod.yml run --rm certbot \
-  certonly --webroot \
-  --webroot-path=/var/www/certbot \
-  --email $EMAIL \
-  --agree-tos \
-  --no-eff-email \
-  -d $DOMAIN \
-  -d www.$DOMAIN
+echo ">>> Waiting for Nginx"
+sleep 5
 
-# Start everything
+echo ">>> Issuing certificate"
+docker compose -f docker-compose.prod.yml run --rm \
+  --entrypoint "certbot certonly --webroot \
+    --webroot-path=/var/www/certbot \
+    --email ${EMAIL} \
+    --agree-tos \
+    --no-eff-email \
+    -d ${DOMAIN} \
+    -d www.${DOMAIN}" \
+  certbot
+
+echo ">>> Restoring SSL Nginx config"
+cp docker/nginx/nginx.conf.backup docker/nginx/nginx.conf
+
+echo ">>> Restarting all services"
 docker compose -f docker-compose.prod.yml up -d
+
+echo ">>> Done"
